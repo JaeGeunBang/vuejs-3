fb219cb836849ef77b64a0645ecc25d5b92bc358
JaeGeunBang <jgb710@naver.com>
2022-01-07 16:08:41 +0900

    search 검색 기능 추가 (computed를 통해 filtered 된 todos를 가짐)

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+    <input
+        class="form-control"
+        type="text"
+        v-model="searchText"
+        placeholder="Search"
+    >
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
 
-    <div v-if="!todos.length">추가된 Todo가 없습니다.</div>
-    <TodoList :todos_data="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+    <div v-if="!filteredTodos.length">추가된 Todo가 없습니다.</div>
+    <TodoList :todos_data="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm'
 import TodoList from './components/TodoList'
 
@@ -41,12 +48,25 @@ export default {
       todos.value[index].completed = !todos.value[index].completed
     }
 
+    const searchText = ref('')
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value)
+        });
+      }
+
+      return todos.value
+    })
+
     return {
       todoStyle,
       todos,
       addTodo,
       deleteTodo,
       toggleTodo,
+      searchText,
+      filteredTodos,
     }
   }
 }
