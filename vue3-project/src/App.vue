3c16ce3dfbb8d65f143cee581814041f6d7e4130
JaeGeunBang <jgb710@naver.com>
2022-01-08 13:10:49 +0900

    pagination 기능 추가

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -13,6 +13,27 @@
 
     <div v-if="!filteredTodos.length">추가된 Todo가 없습니다.</div>
     <TodoList :todos_data="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage - 1)">
+            Previous
+          </a>
+        </li>
+        <li
+            v-for="page in numberOfPages"
+            :key="page"
+            class="page-item"
+            :class="currentPage === page ? 'active' : ''"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{page}}</a>
+        </li>
+        <li v-if="numberOfPages !== currentPage" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -30,23 +51,28 @@ export default {
   setup() {
     const todos = ref([])
     const error = ref('')
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
 
-    const getTodos = async () => {
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
       try {
-        const res = await axios.get('http://localhost:3000/todos')
-        todos.value = res.data
-      } catch(err) {
-        console.log(err)
-        error.value = "[GET] Something went wrong."
+        const res = await axios.get(
+            `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
+        );
+        numberOfTodos.value = res.headers['x-total-count'];
+        todos.value = res.data;
+      } catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong.';
       }
     }
 
     getTodos();
-
-    const todoStyle = {
-      textDecoration: 'line-through',
-      color: 'gray'
-    }
 
     const addTodo = async (todo) => {
       error.value = '';
@@ -98,7 +124,6 @@ export default {
     })
 
     return {
-      todoStyle,
       todos,
       addTodo,
       deleteTodo,
@@ -106,6 +131,9 @@ export default {
       searchText,
       filteredTodos,
       error,
+      numberOfPages,
+      currentPage,
+      getTodos,
     }
   }
 }
