e8d12ac24d59167db3c3cb26db6cde31f758f1e4
JaeGeunBang <jgb710@naver.com>
2022-01-12 21:23:55 +0900

    quasar 설정 및 뉴스 정보 불러오기

--- quasar-news-project/src/pages/Index.vue
+++ quasar-news-project/src/pages/Index.vue
@@ -1,49 +1,36 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <div>
+    <q-list bordered>
+      <q-item clickable v-ripple
+              v-for="news in news_list"
+              :key="news.link"
+      >
+        <q-item-section class="text-h6"> {{ news.title }} </q-item-section>
+        <q-item-section side class="text-caption"> {{ news.pubDate }} </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from 'vue';
+import axios from 'axios'
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex',
-  components: { ExampleComponent },
+export default {
   setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
+    let news_list = ref([]);
+    const getNews = async () => {
+      const rssURL: string = "https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko"
+      const apiURL: string = 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(rssURL) ;
+      const response = await axios.get(apiURL) ;
+      news_list.value = response.data.items
+    }
+    getNews()
+
+    return {
+      news_list,
+      getNews,
+    }
   }
-});
+}
 </script>
