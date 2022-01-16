06e8314cd3f191f1a7c6d36db99c810a72acc7ac
JaeGeunBang <jgb710@naver.com>
2022-01-16 15:21:42 +0900

    router 설정, qtable, qdrawer 활용

--- quasar-news-project/src/layouts/MainLayout.vue
+++ quasar-news-project/src/layouts/MainLayout.vue
@@ -1,117 +1,78 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr lff" container style="height: 300px" class="shadow-2 rounded-borders">
+    <q-header elevated class="bg-black">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <q-toolbar-title>뉴스 리스트</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="drawer"
       show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+
+      :width="200"
+      :breakpoint="500"
+      bordered
+      class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <router-link to="/">
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                뉴스 리스트
+              </q-item-section>
+            </q-item>
+          </router-link>
+
+          <router-link to="/about">
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="home" />
+              </q-item-section>
+
+              <q-item-section>
+                About
+              </q-item-section>
+            </q-item>
+          </router-link>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
+
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-import { defineComponent, ref } from 'vue'
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
+export default {
   setup () {
-    const leftDrawerOpen = ref(false)
-
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      drawer: ref(false),
+      miniState: ref(true)
     }
   }
-})
+}
 </script>
+
+<style lang="scss">
+  a:link, a:hover, a:active, a:visited {
+    text-decoration: none;
+    color: black
+  }
+</style>
--- quasar-news-project/src/pages/Index.vue
+++ quasar-news-project/src/pages/Index.vue
@@ -1,35 +1,94 @@
 <template>
-  <div>
-    <q-list bordered>
-      <q-item clickable v-ripple
-              v-for="news in news_list"
-              :key="news.link"
-      >
-        <q-item-section class="text-h6"> {{ news.title }} </q-item-section>
-        <q-item-section side class="text-caption"> {{ news.pubDate }} </q-item-section>
-      </q-item>
-    </q-list>
+  <div class="q-pa-md">
+    <q-table
+      flat
+      :rows="news_list"
+      :columns="columns"
+      row-key="title"
+      :filter="filter"
+      @row-click="rowClick"
+      :pagination="initialPagination"
+    >
+      <template v-slot:body-cell-title="props">
+        <q-td :prop="props">
+          <div class="text-h6" style="white-space: normal">
+            {{ props.value }}
+          </div>
+          <div class="my-table-details">
+            {{ props.row.details }}
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-link="props">
+        <q-td :prop="props">
+          <div style="white-space: normal">
+            {{ props.value }}
+          </div>
+          <div class="my-table-details">
+            {{ props.row.details }}
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-pubDate="props">
+        <q-td :prop="props">
+          <div>
+            {{ props.value }}
+          </div>
+          <div class="my-table-details">
+            {{ props.row.details }}
+          </div>
+        </q-td>
+      </template>
+      <template>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script lang="ts">
 import axios from 'axios'
 import { ref } from 'vue';
+import { Notify } from 'quasar'
 
 export default {
   setup() {
     let news_list = ref([]);
+    const filter = ref('');
+    const columns = [
+      { name: 'title', required: true, label: '기사 제목', align: 'left', field: 'title' },
+      { name: 'pubDate', align: 'left', label: '발행일', field: 'pubDate', sortable: true },
+    ]
+
     const getNews = async () => {
       const rssURL: string = "https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko"
       const apiURL: string = 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(rssURL) ;
       const response = await axios.get(apiURL) ;
       news_list.value = response.data.items
+      Notify.create({message:'뉴스를 성공적으로 불러왔습니다'})
     }
     getNews()
+
+    const rowClick = (event: any, row: any) => {
+      window.open(row.link)
+    }
 
     return {
       news_list,
       getNews,
+      columns,
+      filter,
+      rowClick,
+      initialPagination: {
+        sortBy: 'desc',
+        descending: false,
+        page: 0,
+        rowsPerPage: 3
+      }
     }
   }
 }
